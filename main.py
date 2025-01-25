d49e0413e451ee711b2a414ace0dd1f2cfa7d649
saleh <saleh@salehs-MacBook-Air.local>
2025-01-25 13:05:34 +0330

    pushing

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from Colorizer import *
 
 colorizer = Colorizer(width= 640, height= 480)
 
-colorizer.processImage("inputs/test.png")
-colorizer.processVideo("inputs/test2.mp4")
+colorizer.processImage("inputs/test.webp")
+colorizer.processVideo("inputs/test.mp4")
